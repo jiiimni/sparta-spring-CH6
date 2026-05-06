d625e7480ad53758dc68a0cf18d3d6bfe9b31e2c
jiiimni <leejimin8951@naver.com>
2026-05-06 19:49:25 +0900

    chore: 프로젝트 초기 세팅

---
+++ sparta-spring-CH6/src/main/java/kr/spartaclub/spartaspringch6/SpartaSpringCh6Application.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.spartaspringch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaSpringCh6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSpringCh6Application.class, args);
+    }
+
+}
---
+++ sparta-spring-CH6/src/test/java/kr/spartaclub/spartaspringch6/SpartaSpringCh6ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.spartaspringch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSpringCh6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
